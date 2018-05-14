98688eba50001e02ea89837dc84a324e62d7c20a
Jack D <dmarkov23@gmail.com>
2018-05-14 14:05:49 +0200

    path fix of serving index

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, '/dist')));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/practical-automata-client/index.html'));
 });
 
 console.log("Express server running");
