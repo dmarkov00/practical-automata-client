1796d3b90520a73ea155eb615a7d4ca6dec86e89
Jack D <dmarkov23@gmail.com>
2018-05-14 14:15:36 +0200

    deployment path fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '/dist/practical-automata-client')));
 
 app.listen(process.env.PORT || 8080);
 
